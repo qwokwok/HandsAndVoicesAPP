8756d87982ac2de59eada3b6385deed96e22b83a
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-09-12 15:41:20 -0400

    added ffloading init for ios

--- HandsAndVoices/HandsAndVoices.iOS/Main.cs
+++ HandsAndVoices/HandsAndVoices.iOS/Main.cs
@@ -13,8 +13,15 @@ namespace HandsAndVoices.iOS
         static void Main(string[] args)
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            // you can specify it here
+            try
+            {
+                UIApplication.Main(args, null, "AppDelegate");
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
