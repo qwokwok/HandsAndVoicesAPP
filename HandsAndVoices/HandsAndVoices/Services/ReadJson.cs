51e1b9b152d6d97a98ddfc4c9b897a43b4b2ccf6
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2020-09-27 02:05:23 -0400

    fixing days

--- HandsAndVoices/HandsAndVoices/Services/ReadJson.cs
+++ HandsAndVoices/HandsAndVoices/Services/ReadJson.cs
@@ -54,10 +54,14 @@ namespace HandsAndVoices.Services
 
                 // decreases by 4 hours to get time at eastern standard zone
                 var current = now - new TimeSpan(4, 0, 0);
-
-                App.DayCount = (int)(Math.Floor((current - App.FirstTime).TotalDays) + 1);
+                var totalDays = (current - App.FirstTime).TotalDays; 
+                App.DayCount = (int)Math.Floor(totalDays + 1);
                 if (App.DayCount == 0)
+                {
                     App.DayCount = 1;
+                    var oneDayMore = App.FirstTime - new TimeSpan(24, 0, 0);
+                    Preferences.Set("date_key", oneDayMore);
+                }
 
                 //App.DayCount = 90; // If you want to test with any number of days have been past, enter 1-90 or comment out this line if you wish to test with actual day.
                 // --- ---
